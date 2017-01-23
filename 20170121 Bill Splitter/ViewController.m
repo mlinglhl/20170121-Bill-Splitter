339aff523318c1202ee41e5c59891e0adea0fba0
mlinglhl <minhung.ling@gmail.com>
2017-01-23 09:18:48 -0500

    Final commit

--- 20170121 Bill Splitter/ViewController.m
+++ 20170121 Bill Splitter/ViewController.m
@@ -9,16 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UISlider *slider;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UILabel *sliderLabel;
+@property NSNumberFormatter *formatter;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.formatter = [NSNumberFormatter new];
+    [self.formatter setNumberStyle:NSNumberFormatterCurrencyStyle];
 }
 
+- (IBAction)calculate:(UIButton *)sender {
+    float cost = [self.textField.text floatValue];
+    self.label.text = [self.formatter stringFromNumber:@(cost/self.slider.value)];
+}
+
+- (IBAction)changePeople:(UISlider *)sender {
+    self.slider.value = round(self.slider.value);
+    self.sliderLabel.text = [NSString stringWithFormat:@"%.0f", self.slider.value];
+    float cost = [self.textField.text floatValue];
+    self.label.text = [self.formatter stringFromNumber:@(cost/self.slider.value)];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
